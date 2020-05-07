b14ea89180085f8ecca70646084ccd997ef0e349
Valentina Panic <valentina.panic87@gmail.com>
2020-05-07 03:16:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,9 @@ def speak_to_grandma(phrase)
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.downcase
     print "HUH?! SPEAK UP, SONNY!"
+  else
+    puts " NO, NOT SINCE 1938!"
+  end
 
 
 end
