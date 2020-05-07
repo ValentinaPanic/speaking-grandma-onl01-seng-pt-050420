2d646e586195828ad54b1760eb37dcff422238f8
Valentina Panic <valentina.panic87@gmail.com>
2020-05-07 04:02:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,6 @@
 def speak_to_grandma(string)
-  if string == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  elsif string == string.downcase
-    puts "HUH?! SPEAK UP, SONNY!"
-  else
-    puts " NO, NOT SINCE 1938!"
+  if string == string.upcase
+    puts "HUH?! SPEAK UP,SONNY!"
   end
 end
 
